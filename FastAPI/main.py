0e2806746730b87566813ceee12d1ecc5794608a
LeeInGyu <developLee20@gmail.com>
2024-03-30 22:43:22 +0900

    FastAPI 쿼리 매개변수 part 공부

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -2,31 +2,22 @@ from fastapi import FastAPI
 from typing import *
 from enum import Enum
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
 app = FastAPI()
 
-@app.get("/items/{item_id}")
-async def root(item_id: int):
-    return {"item_id" : item_id}
-
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int,
+    item_id: str,
+    q: Union[str, None],
+    short: bool = False):
     
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-    
-    return {"model_name": model_name, "message": "Have some residuals"}
+    item = {"item_id": item_id, "owner_id": user_id}
+
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item."}
+        )
+
+    return item
